c6aca0699120ab531fa7a24e5c44e2e89e159d09
王越 <1414101921@qq.com>
2024-03-27 11:15:22 +0800

    feat: ✨ 无头浏览器

---
+++ app/Console/Commands/ChromeHeadless.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Console\Commands;
+
+use HeadlessChromium\BrowserFactory;
+use Illuminate\Console\Command;
+
+class ChromeHeadless extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'other:chrome-headless';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = '无头浏览器';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+
+        $browserFactory = new BrowserFactory();
+
+        $browserFactory->setOptions([
+            'windowSize' => [1920, 1000],
+        ]);
+
+        $browserFactory->addOptions(['noSandbox' => true]);
+        $browserFactory->addOptions(['enableImages' => true]);
+
+        $browser = $browserFactory->createBrowser();
+
+        $page = $browser->createPage();
+
+        $page->navigate('https://www.baidu.com/')->waitForNavigation();
+
+        $page->screenshot()->saveToFile('test.png');
+
+        $browser->close();
+
+    }
+}
